df107087f54183b383d8fedf9e2ed77bb872d38c
gitsaman16 <roopa.arjunan@gmail.com>
2019-03-09 00:43:22 -0500

    first changes

---
+++ src/test/java/Test/DemoTest.java
@@ -0,0 +1,14 @@
+package Test;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test1()
+	{
+		Reporter.log("sfgagfdg", true);
+	}
+}
